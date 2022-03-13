f33afc57c232cc50b0a0a11858f09824a457d1b4
Konstantin <konstantin@stark.upc.de>
2022-03-13 11:22:27 +0100

    some changes

--- memrise/sort.py
+++ memrise/sort.py
@@ -8,14 +8,20 @@ if len(sys.argv) != 2:
 file_to_be_sorted = str(sys.argv[1])
 
 words = []
+with open(file_to_be_sorted, "r+") as file:
+    lines = file.readlines()
+    lines.sort()
+    file.seek(0)
+    file.writelines(lines)
 
-with open(file_to_be_sorted) as file:
-    for line in file:
-        list_tmp = [elt.strip() for elt in line.split(' ')]
-        words.extend(list_tmp)
+# with open(file_to_be_sorted) as file:
+#     for line in file:
+#         list_tmp = [elt.strip() for elt in line.split('\n')]
+#         if len(list_tmp) == 0 or list_tmp[0] == '': continue
+#         words.extend(list_tmp)
 
-words = sorted(words)
-print(words)
-with open(file_to_be_sorted+'_sorted', 'a') as file:
-    for word in words:
-        file.write(word+'\n')
+# words = sorted(words, key=lambda words:d[words[0]])
+# print(words[:100])
+# with open(file_to_be_sorted+'_sorted', 'a') as file:
+#     for word in words:
+#         file.write(word+'\n')

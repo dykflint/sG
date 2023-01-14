ee2b8ae858b6886eab68bcdf7e1f6a1f49dc1c9a
Konstantin <konstantin@jarvis.local>
2023-01-14 14:53:13 -0500

    Clean Up the lid folders

--- lid/all_questions/run_conversion.sh
+++ lid/all_questions/run_conversion.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-let start_question=11
-let number_of_questions=20
-for i in {11..30}
+let start_question=1
+let number_of_questions=10
+for i in {1..10}
 do
 	python3 convert_questions.py german/$i english/$i $start_question $i $number_of_questions
 done

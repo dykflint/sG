5d2aa9619983f9ed1001057264324424e648e832
DykFlint <konstantin.tamoev@gmail.com>
2023-02-08 15:07:37 +0100

    Create RC Schreiben Teil 1

---
+++ tools/a1Tools/schreibenTeil2/script.js
@@ -0,0 +1,4 @@
+// CONSTANTS
+const iframeContainer = document.querySelector('.babystepsIframe');
+
+iframeContainer.setAttribute("src", "https://smartergerman.github.io/babystepstool/a1/5/index.html")
